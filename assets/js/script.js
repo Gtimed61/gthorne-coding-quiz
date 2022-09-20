3188b6f9c36efe38c470a6d1d3917bd1478fe5a0
Thorne <gthorne61@gmail.com>
2022-09-19 20:03:07 -0500

    tutoring help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 var quizContainer = document.getElementById("quiz")
-var startGame = document.getElementById("start-quiz")
+var buttonEl = document.querySelector("#start-btn")
 var resultsContainer = document.getElementById("player-score")
 var highScore = document.getElementById("high-score")
-var endGame = 
+var timerCount = 60
+var timeEl = document.getElementById("timer")
+var choiceA = document.getElementById("choice-a")
+var choiceB = document.getElementById("choice-b")
+var choiceC = document.getElementById("choice-c")
+var choiceD = document.getElementById("choice-d")
 
 var questions = [
     {
@@ -42,29 +47,59 @@ var questions = [
     },        
 ]
 
-// timer for start and stop
-var startGame = function(event) {
-    document.querySelector("start-quiz");
-    var quizContainer = document.querySelector("quiz");
-    var questions = document.querySelector("questions");
+choiceA.style.display = "none"
+choiceB.style.display = "none"
+choiceC.style.display = "none"
+choiceD.style.display = "none"
+// start game and load questions
+function startGame() { 
+    startTimer()
+    choiceA.style.display = ""
+    choiceB.style.display = ""
+    choiceC.style.display = ""
+    choiceD.style.display = ""
+    cycleQuestions(0)
+
 }
 
+function startTimer() {
+    timeEl.textContent = timerCount
+    console.log(timeEl.textContent)
+    var timer = setInterval(function (){
+    timeEl.textContent --;
+    if(timeEl.textContent === "0"){
+        clearInterval(timer)
+    }
+    },1000)
+}
+function cycleQuestions(index) {
+    var questionSection = document.getElementById("questions")
+    questionSection.textContent = questions[index].question
+    choiceA.textContent = questions[index].answerChoices[0]
+    choiceB.textContent = questions[index].answerChoices[1]
+    choiceC.textContent = questions[index].answerChoices[2]
+    choiceD.textContent = questions[index].answerChoices[3]
+}
 // end game function with scoring and player input
-var endGame = function() {
-    
-    var highScore = localStorage.getItem("highscore");
-    
-    if (highScore === null) {   
-        highScore = 0;
+function endGame() {
+
+    var highScore = localStorage.getItem("highscore")
+
+    if (highScore === null) {
+        highScore = 0
     }
-    
+
     if (playerScore > highScore) {
-    localStorage.setItem("highScore");
-    localStorage.setItem("Player-initials");
+        localStorage.setItem("highScore")
+        localStorage.setItem("Player-initials")
     }
 }
 
-addEventListener// elements for the buttons to click
+
+buttonEl.addEventListener("click", startGame)
+
+
+// elements for the buttons to click
 // function to end game after time runs out or the there are no more questions
 // local storage for the save initials and high score
 // incorrect answer subtracts from time
