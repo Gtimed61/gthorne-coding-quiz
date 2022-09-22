84614730b7fb7fc0988b9320b5581bcb9de9b056
Thorne <gthorne61@gmail.com>
2022-09-22 16:34:08 -0500

    turning in need help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//var for use
 var quizContainer = document.getElementById("quiz")
 var buttonEl = document.querySelector("#start-btn")
 var resultsContainer = document.getElementById("player-score")
@@ -8,7 +9,7 @@ var choiceA = document.getElementById("choice-a")
 var choiceB = document.getElementById("choice-b")
 var choiceC = document.getElementById("choice-c")
 var choiceD = document.getElementById("choice-d")
-
+//array for questions
 var questions = [
     {
         question: "what code is the main structure of a webpage?",
@@ -46,7 +47,7 @@ var questions = [
             
     },        
 ]
-
+//display choices for questions
 choiceA.style.display = "none"
 choiceB.style.display = "none"
 choiceC.style.display = "none"
@@ -54,14 +55,14 @@ choiceD.style.display = "none"
 // start game and load questions
 function startGame() { 
     startTimer()
-    choiceA.style.display = ""
-    choiceB.style.display = ""
-    choiceC.style.display = ""
-    choiceD.style.display = ""
+    choiceA.style.display = "none"
+    choiceB.style.display = "none"
+    choiceC.style.display = "none"
+    choiceD.style.display = "none"
     cycleQuestions(0)
 
 }
-
+// timer start function
 function startTimer() {
     timeEl.textContent = timerCount
     console.log(timeEl.textContent)
@@ -72,6 +73,7 @@ function startTimer() {
     }
     },1000)
 }
+//function to cycle questions
 function cycleQuestions(index) {
     var questionSection = document.getElementById("questions")
     questionSection.textContent = questions[index].question
@@ -79,6 +81,7 @@ function cycleQuestions(index) {
     choiceB.textContent = questions[index].answerChoices[1]
     choiceC.textContent = questions[index].answerChoices[2]
     choiceD.textContent = questions[index].answerChoices[3]
+
 }
 // end game function with scoring and player input
 function endGame() {
@@ -95,7 +98,7 @@ function endGame() {
     }
 }
 
-
+//start button click event listener
 buttonEl.addEventListener("click", startGame)
 
 
